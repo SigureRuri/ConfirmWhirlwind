3a6cbbec8651d9ca3622c8446e30b8e3735bfa2d
Lucky <Lucky3028@users.noreply.github.com>
2022-06-15 13:38:35 +0900

    fix: 謎の誤字を修正
    Co-authored-by: SigureRuri <50079507+SigureRuri@users.noreply.github.com>

--- src/main/kotlin/com/github/sigureruri/confirmwhirlwind/ConfirmWhirlwind.kt
+++ src/main/kotlin/com/github/sigureruri/confirmwhirlwind/ConfirmWhirlwind.kt
@@ -48,7 +48,7 @@ class ConfirmWhirlwind : JavaPlugin(), Listener {
                 "${ChatColor.DARK_RED}！！  ${ChatColor.RED}${ChatColor.BOLD}確認  ${ChatColor.DARK_RED}！！",
                 " ${ChatColor.RED}間違い防止の為、もう一度",
                 " ${ChatColor.WHITE}[${executedCommand}]${ChatColor.RED}を実行してください。"
-            ).forEach { context.sender.sendMessage(it) }
+            ).forEach { player.sendMessage(it) }
         }
     }
 
